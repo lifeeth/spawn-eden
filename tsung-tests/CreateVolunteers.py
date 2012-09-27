cbfbca196bb2c52eb2fb5651b010be919b8dd140
Praneeth <lifeeth@gmail.com>
2012-09-27 16:11:11 -0400

    compatible with rev - 9ae96381a0e49543309e560d0a71d154e1490c6a

--- tsung-tests/CreateVolunteers.py
+++ tsung-tests/CreateVolunteers.py
@@ -2,7 +2,7 @@ import csv
 import copy
 with open('volunteers.csv', 'wb') as csvfiles:
     writer = csv.writer(csvfiles, delimiter=',')
-    baseline = ['34','','FirstName','MiddleName','LastName','2-Jan-1987','3','FirstName.LastName@exammple.com','1234567890','','16','01-Jan-2012','31-Dec-2012','dummy','230','BuildingName','StreetAddress','560062','State','','City','','','','240']
+    baseline = ['34','','FirstName','MiddleName','LastName','2-Jan-1987','3','FirstName.LastName@exammple.com','1234567890','','26','01-Jan-2012','31-Dec-2012','dummy','230','BuildingName','StreetAddress','560062','State','','City','','','','240']
     for i in range(1000):
         templine = copy.deepcopy(baseline)
         templine[2] = str(i) + baseline[2]
