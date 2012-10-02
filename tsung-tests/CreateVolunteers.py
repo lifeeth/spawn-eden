1662bbdf7509f4774a535986d1383521ffe3e614
Praneeth <lifeeth@gmail.com>
2012-10-02 15:41:29 -0400

    More fine grained tests with rampup

--- tsung-tests/CreateVolunteers.py
+++ tsung-tests/CreateVolunteers.py
@@ -3,7 +3,7 @@ import copy
 with open('volunteers.csv', 'wb') as csvfiles:
     writer = csv.writer(csvfiles, delimiter=',')
     baseline = ['34','','FirstName','MiddleName','LastName','2-Jan-1987','3','FirstName.LastName@exammple.com','1234567890','','26','01-Jan-2012','31-Dec-2012','dummy','230','BuildingName','StreetAddress','560062','State','','City','','','','240']
-    for i in range(1000):
+    for i in range(10000):
         templine = copy.deepcopy(baseline)
         templine[2] = str(i) + baseline[2]
         templine[7] = str(i) + baseline[7]
